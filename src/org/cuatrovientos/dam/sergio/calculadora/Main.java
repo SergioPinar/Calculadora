6d31b545b5228347a0ee5b713099bd6688d81d60
Sergio <sergiopi137@gmail.com>
2025-01-20 19:11:13 +0100

    Introducción código

--- src/org/cuatrovientos/dam/sergio/calculadora/Main.java
+++ src/org/cuatrovientos/dam/sergio/calculadora/Main.java
@@ -1,10 +1,48 @@
 package org.cuatrovientos.dam.sergio.calculadora;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Introduce la operación (suma, resta, multiplicacion, division): ");
+        String operacion = scanner.nextLine().toLowerCase();
 
-	}
+        System.out.print("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
 
+        System.out.print("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        double resultado = 0;
+
+        switch (operacion) {
+            case "suma":
+                resultado = num1 + num2;
+                break;
+            case "resta":
+                resultado = num1 - num2;
+                break;
+            case "multiplicacion":
+                resultado = num1 * num2;
+                break;
+            case "division":
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Error: No se puede dividir entre 0");
+                    scanner.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operación no válida");
+                scanner.close();
+                return;
+        }
+
+        System.out.println("El resultado de la " + operacion + " es: " + resultado);
+        scanner.close();
+    }
 }
